5acfe1be65ec5337a233ffd481823d79ba122641
Jeff Clare <99364326+cx-jeff-clare@users.noreply.github.com>
2026-02-06 15:02:03 -0500

    Test Stage/Commit

--- src/web/services.py
+++ src/web/services.py
@@ -5,6 +5,8 @@ from django.contrib.auth.backends import BaseBackend
 from django.contrib.auth.models import User
 from django.db import connection, transaction
 
+
+
 from web.models import (
     Account,
     CashAccount,
